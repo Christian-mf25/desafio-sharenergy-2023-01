338e39fe1f1d748f46c90262317043e93778a392
Christian <christian.mf22@gmail.com>
2023-01-05 20:36:54 -0300

    feat: Create customer controller

---
+++ back/src/controller/customer/getAllCustomer.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { CustomerRepository, ICustomer } from "../../repository";
+
+const getAllCustomerController = async (_: Request, res: Response) => {
+  const customer: ICustomer[] = await new CustomerRepository().findCustomers();
+
+  return res.status(200).json(customer);
+};
+
+export default getAllCustomerController;
