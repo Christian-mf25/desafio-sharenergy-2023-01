f8e8d68cd9db9023ff6fca795c471a9b5ba2168d
Christian <christian.mf22@gmail.com>
2023-01-06 20:06:42 -0300

    feat: Added middleware to the upgrade path

--- back/src/routes/Customer/customer.routes.ts
+++ back/src/routes/Customer/customer.routes.ts
@@ -12,14 +12,10 @@ import { checkDuplicateCustomer, validateShape } from "../../middlewares";
 
 const routesCustomer = Router();
 
-routesCustomer.post("", checkDuplicateCustomer,validateShape(customerShape), createCustomerController);
+routesCustomer.post("", checkDuplicateCustomer, validateShape(customerShape), createCustomerController);
 routesCustomer.get("", getAllCustomerController);
 routesCustomer.get("/:email", getOneCustomerController);
-routesCustomer.patch(
-  "/:email",
-  validateShape(userUpdateShape),
-  updateCustomerController
-);
+routesCustomer.patch("/:email", checkDuplicateCustomer, validateShape(userUpdateShape), updateCustomerController);
 routesCustomer.delete("/:email", deleteCustomerController);
 
 export default routesCustomer;
