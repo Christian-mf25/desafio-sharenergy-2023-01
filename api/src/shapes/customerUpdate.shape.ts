ceceb1fcb33ba1ed05ce7cdc643babf39ae0837a
Christian <christian.mf22@gmail.com>
2023-01-08 09:09:33 -0300

    fix: Added regex in customerUpdate shape

--- api/src/shapes/customerUpdate.shape.ts
+++ api/src/shapes/customerUpdate.shape.ts
@@ -1,11 +1,31 @@
 import * as yup from "yup";
 
+import { makeTitle } from "../utils";
+
 const userUpdateShape = yup.object().shape({
-  name: yup.string().optional(),
-  email: yup.string().email().lowercase().optional(),
-  phone: yup.string().optional(),
-  address: yup.string().optional(),
-  cpf: yup.string().optional(),
+  name: yup
+    .string()
+    .optional()
+    .transform((str) => makeTitle(str)),
+  email: yup.string().email("Invalid e-mail").lowercase().optional(),
+  phone: yup
+    .string()
+    .optional()
+    .matches(
+      /^\(\d{2}\)\d{4,5}\-\d{4}$/,
+      "Accepted phone format (xx)xxxxx-xxxx"
+    ),
+  address: yup
+    .string()
+    .optional()
+    .transform((str) => makeTitle(str)),
+  cpf: yup
+    .string()
+    .optional()
+    .matches(
+      /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/,
+      "Accepted cpf format xxx.xxx.xxx-xx"
+    ),
 });
 
 export default userUpdateShape;
